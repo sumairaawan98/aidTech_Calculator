5a56e85804bed800a51f967e755384b4a2a0020b
Sumaira Saeed Awan <60813545+sumairaawan98@users.noreply.github.com>
2023-12-07 17:58:35 +0500

    Add files via upload

---
+++ method.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+function f1() { 
+const x1=parseInt( document.getElementById("X").value)
+const x2=parseInt(document.getElementById("Y").value)
+const result=x1+x2; 
+document.getElementById("result").value = result
+} 
+
+function f2() { 
+const x1=parseInt( document.getElementById("X").value)
+const x2=parseInt(document.getElementById("Y").value)
+const result=x1-x2; 
+document.getElementById("result").value = result
+} 
+function f3() { 
+const x1=parseInt( document.getElementById("X").value)
+const x2=parseInt(document.getElementById("Y").value)
+const result=x1*x2; 
+document.getElementById("result").value = result
+} 
+
+function f4() { 
+const x1=parseInt( document.getElementById("X").value)
+const x2=parseInt(document.getElementById("Y").value)
+const result=x1/x2; 
+document.getElementById("result").value = result 
+} 
